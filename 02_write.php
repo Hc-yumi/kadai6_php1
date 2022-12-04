520eb123659a917a2a8c2e2a8de908d43fd02d5c
Hc-yumi <hach8686@gmail.com>
2022-12-04 18:35:42 +0900

    1204

--- 02_write.php
+++ 02_write.php
@@ -3,14 +3,40 @@
 $name = $_POST['name'];
 $mail = $_POST['mail'];
 $birthPlace = $_POST['birthPlace'];
+$cs = $_POST['cs'];
+$q = $_POST['q'];
 
-$time = date('Y-m-d H:i:s');
+// $time = date('Y-m-d H:i:s');
+
+
+// ラジオボタンの確認
+if(isset($_POST['cs'])){
+  $cs =$_POST['cs'];
+  echo '満足度:' . $cs .'<br>';
+} else {
+  echo '満足度が選択されていません。<br>';
+}
+
+
+// チェックボックスの確認
+if (isset($_POST['q'])) {
+  $q = implode(", ", $_POST["q"]);
+  echo '利用の目的：' . $q . '<br>';
+} else {
+  echo 'チェックされていません。<br>';
+}
+
+
 
 // ファイルに書き込み
 // ①開く
 $file = fopen('data/data.txt', 'a');
 // ②書く
-fwrite($file, $name. $mail. $birthPlace. $time."\n");
+// fwrite($file, $name." " .$mail." " .$birthPlace. " ".$cs." " .$q." " .$time. "\n")
+// $file_w = array($name,$mail,$birthPlace,$cs,$q);
+// fwrite($file, $file_w);
+fwrite($file, $name.' , ' .$mail.' , ' .$birthPlace. ' , '.$cs.' , ' .$q."\n");
+// fwrite($file, $name. $mail. $birthPlace. $cs. $q."\n");
 // ③閉じる
 fclose($file);
 //文字作成
--- 03_read.php
+++ 03_read.php
@@ -1,30 +1,65 @@
-<?php
-// ファイルを開く
-$openFile = fopen('./data/data.txt', 'r');
+      <?php
+          // ファイルを開く
+          $openFile = fopen('./data/data.txt', 'r');
+          
+          
+          // ファイル内容を1行ずつ読み込んで出力
+          // ----以下講義の書き方------//
+          // while ($str = fgets($openFile)) {
+          //   echo nl2br($str); 
+          // };
+            
+          if (!$openFile){
+            echo "Somehow the file cannot be opened! :)";
+            exit;
+          }
+          
+          echo "<table border = 4>";
+          $counter = 1;
+          while(!feof($openFile))
+          {
+            $zeile = fgets($openFile);
+            // ,を｜に変える
+            // $str= str_replace(' , ' , ' | ' ,$zeile);
+            // echo nl2br($str);
+            echo "<tr><td>$counter</td>";
+            echo "<td>$zeile</td>";
+            $counter++;
 
-// ファイル内容を1行ずつ読み込んで出力
-while ($str = fgets($openFile)) {
-  echo nl2br($str); 
-};
+            var_dump($zeile);
+          }
+            echo "</table>";
+  
 
-// ファイルを閉じる
-fclose($openFile);
+           // $file = fopen("info.txt", "r") or die("Unable to open file!");
+          // while (!feof($openFile)){   
+          // $data = fgets($openFile); 
 
-?>
+          // echo "<tr><td>" . str_replace('|','</td><td>',$data) . '</td></tr>';
+      //     $str = str_replace(" ", " | ", $openFile); 
+      //           echo nl2br($str);
+      // }
 
 
+      // echo '</table>';
 
-<html>
+      // ファイルを閉じる
+      fclose($openFile);
 
-<head>
-    <meta charset="utf-8">
-    <title>readFileの確認</title>
-</head>
+      ?>
 
-<body>
 
-  <p><a href="01_post.php">アンケートに戻る</a></p>
+  <html>
 
-</body>
+  <head>
+      <meta charset="utf-8">
+      <title>readFileの確認</title>
+  </head>
+
+  <body>
+
+    <p><a href="01_post.php">アンケートに戻る</a></p>
+
+  </body>
 
 </html>
